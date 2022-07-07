8c651df60a9764f01280a8cdd234eb4ee5a1bdbb
Oruç Han Kural <77747739+oruchankural@users.noreply.github.com>
2022-07-08 02:22:23 +0300

    Add project files.

---
+++ GarbageCollector/ParamKeyword.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector
+{
+    public class ParamKeyword
+    {
+        // ll be return result
+        public int result { get; }
+
+        // result = param int[] numbers > goes on summarize method
+        public ParamKeyword(params int[] numbers)
+        {
+            result = summarize(numbers);
+        }
+
+        public static int summarize(params int[] numbers)
+        {
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total = total + number;
+            }
+
+            numbers = null;
+
+            return total;
+        }
+    }
+   
+}
---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+
+            // Disposed Event
+            /*
+            Test test = new Test();
+            GC.Collect();
+            test.Dispose();
+            Console.ReadLine();
+             */
+
+
+            // Using param keyword for a class that returns params [] numbers summarized value 
+            /*
+            ParamKeyword pk = new ParamKeyword(1,1,3);
+            var result = pk.result;
+             */
+
+        }
+
+
+
+
+        class Test : IDisposable
+        {
+            protected virtual void Dispose(bool disposing) // can override
+            {
+                    if (disposing)
+                    {
+                          //managed code
+                         //new keyworded objects
+                    }
+                    //unmanager codes
+            }
+
+            ~Test()
+            {
+                Dispose(false);
+            }
+
+            public void Dispose()
+            {
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+}
